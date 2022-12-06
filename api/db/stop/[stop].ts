b4d8a82fe2d99c3f435b00bc337e48ebe28add3a
ornato-t <tommy.ornato@gmail.com>
2022-12-06 11:43:03 +0100

    Trying to add database

--- api/db/stop/[stop].ts
+++ api/db/stop/[stop].ts
@@ -1,9 +1,16 @@
+import { MongoClient } from "mongodb"
+
 export const config = {
   runtime: 'experimental-edge',
 };
 
-export default (req: Request) => {
-  const code = parseInt((req.url.match(/\d{1,}/gm) as string[])[0]);
+const client = new MongoClient(process.env.MONGODB_URI as string);
+await client.connect();
+const db = client.db('GTTTools').collection('stops')
 
-  return new Response(JSON.stringify({code: code, cunt: true}));
+export default async (req: Request) => {
+  const code = parseInt((req.url.match(/\d{1,}/gm) as string[])[0]);
+  const res = await db.findOne({ code: code });
+
+  return new Response(JSON.stringify({ code: code, cunt: true, query: res }));
 }
