4e5dbbc8d6286bdce8e2390589b503f3629975ff
ornato-t <tommy.ornato@gmail.com>
2022-12-06 16:26:30 +0100

    Env vars work

--- api/db/stop/[stop].ts
+++ api/db/stop/[stop].ts
@@ -1,17 +1,17 @@
-// import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb"
 
 export const config = {
   runtime: 'experimental-edge',
 };
 
-// const client = new MongoClient(process.env.MONGODB_URI as string);
+const uri = process.env.MONGODB_URI as string;
+const client = new MongoClient(uri);
 
 export default async (req: Request) => {
-  // await client.connect();
-  // const db = client.db('GTTTools').collection('stops')
-  // const code = parseInt((req.url.match(/\d{1,}/gm) as string[])[0]);
-  // const res = await db.findOne({ code: code });
+  await client.connect();
+  const db = client.db('GTTTools').collection('stops')
+  const code = parseInt((req.url.match(/\d{1,}/gm) as string[])[0]);
+  const res = await db.findOne({ code: code });
 
-  // return new Response(JSON.stringify({ code: code, cunt: true, query: res }));
-  return new Response(process.env.TEST);
+  return new Response(JSON.stringify({ code: code, cunt: true, query: res }));
 }
