f0ca7afc922c9cd0991d537ef726b62654770c0c
ornato-t <tommy.ornato@gmail.com>
2022-12-06 01:23:29 +0100

    Stop page url parameter SSR

---
+++ src/routes/stop/[stop]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Stop {data.code}</h1>
+{#each data.stopData as pass}
+	Route {pass.line}
+	{#each pass.realTime as time}
+	<p>{time}</p>	
+	{/each}
+{/each}
