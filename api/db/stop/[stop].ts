a7db7002c602752a915edb10cfbf4af928e3c4ab
ornato-t <tommy.ornato@gmail.com>
2022-12-06 20:42:08 +0100

    Scrapping edge functions, moving to serverless

--- api/db/stop/[stop].ts
+++ api/db/stop/[stop].ts
@@ -1,16 +1,14 @@
 import { MongoClient } from "mongodb"
 
-export const config = {
-  runtime: 'experimental-edge',
-};
-
 const client = new MongoClient(process.env.MONGODB_URI as string);
 
-export default async (req: Request) => {
+export default async function handler(request: Request, response: any) {  //Can't use type Response, it breaks the return value
   await client.connect();
   const db = client.db('GTTTools').collection('stops')
-  const code = parseInt((req.url.match(/\d{1,}/gm) as string[])[0]);
+  const code = parseInt((request.url.match(/\d{1,}/gm) as string[])[0]);
   const res = await db.findOne({ code: code });
 
-  return new Response(JSON.stringify({ code: code, cunt: true, query: res }));
+  response.status(200 as number).json({
+    code: code, cunt: true, query: res
+  });
 }
