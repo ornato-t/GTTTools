6f00e2c98c84a1b75ac92d96eafd88238c478f7d
ornato-t <tommy.ornato@gmail.com>
2022-12-11 16:20:33 +0100

    GPS redirects to different page, query results are SSRed

---
+++ src/routes/stop/gps/[coords]/+page.server.ts
@@ -0,0 +1,37 @@
+// import { MongoClient } from "mongodb"
+// import { MONGODB_URI } from "$env/static/private";
+// import type { stopDB } from '$lib/stopDB';
+import type { PageServerLoad } from './$types';
+
+// const client = new MongoClient(MONGODB_URI as string);
+
+export const load: PageServerLoad = async ({ params }) => {
+    const [lat, lon, acc] = params.coords.split(';');
+    
+    return {
+        lat, lon, acc
+    };
+}
+
+// async function getDB(code: number) {
+//     await client.connect();
+//     const db = client.db('GTTTools').collection('stops');
+
+//     const aggr = [
+//         { '$match': { 'code': code } },
+//         { '$addFields': { 'coords': '$coordinates' } },
+//         { '$project': { '_id': 0, 'coordinates': 0 } },
+//         {
+//             '$addFields': {
+//                 'coordinates.lat': { '$arrayElemAt': ['$coords', 1] },
+//                 'coordinates.lon': { '$arrayElemAt': ['$coords', 0] }
+//             }
+//         },
+//         { '$project': { 'coords': 0 } },
+//     ]
+
+//     const stops = (await db.aggregate(aggr).toArray())[0] as stopDB
+    
+//     return stops;
+// }
+
