488e31c21f1459e4ea00ebc8477dcd533ce215c6
ornato-t <tommy.ornato@gmail.com>
2022-12-11 15:10:18 +0100

    Better position handling using events

--- src/routes/stop/gps/+page.svelte
+++ src/routes/stop/gps/+page.svelte
@@ -3,61 +3,55 @@
 	import Geolocation from 'svelte-geolocation';
 	import { onMount } from 'svelte';
 
-	let coords: [longitude: number, latitude: number];
+	let coords: GeolocationCoordinates;
 	let getPosition = false;
 	let errorFlag = false;
+	let errorCode: number;
 
 	onMount(() => {
 		getPosition = true;
 	});
-
-	function displayErr() {
-		errorFlag = !errorFlag;
-		return '';
-	}
 </script>
-
-{#if errorFlag}
-	<h1 class="font-bold text-xl">Errore</h1>
-{/if}
 
 <Geolocation
 	getPosition
-	bind:coords
-	let:loading
-	let:success
-	let:error
-	let:notSupported
-	options={{ enableHighAccuracy: true }}
->
-	{#if notSupported}
-		{displayErr()}
-		<div class="bg-error text-error-content text-lg">
-			Il dispositivo non supporta la geolocalizzazione
-		</div>
-	{:else}
-		{#if loading}
-			Caricamento...
-		{/if}
-		{#if success}
-			{JSON.stringify(coords)}
-		{/if}
-		{#if error}
-			{displayErr()}
-			{#if error.code === 1}
-				Accesso alla posizione negato
-			{:else if error.code === 2}
-				Posizione non disponibile
-			{:else if error.code === 3}
-				Il dispositivo non ha risposto in tempo (timeout)
-			{/if}
-		{/if}
-	{/if}
-</Geolocation>
+	on:position={(e) => {
+		coords = e.detail.coords;
+	}}
+	on:error={(e) => {
+		//TODO: check for unsupported device error
+		errorFlag = true;
+		errorCode = e.detail.code;	//This isn't an error
+		console.log(e.detail);
+	}}
+/>
 
 {#if errorFlag}
+	<h1 class="font-bold text-xl">Errore</h1>
+	<div class="bg-error text-error-content text-lg">
+		{#if errorCode === 1}
+			Accesso alla posizione negato
+		{:else if errorCode === 2}
+			Posizione non disponibile
+		{:else if errorCode === 3}
+			Il dispositivo non ha risposto in tempo (timeout)
+		{/if}
+	</div>
 	<span
 		>Prova a usare la
 		<a href="/stop/search" class="link" data-sveltekit-preload-data> ricerca testuale</a>
 	</span>
+{:else if coords != undefined}
+	<div>Coordinate</div>
+	<div>
+		Latitudine: {coords.latitude}
+	</div>
+	<div>
+		Longitudine: {coords.longitude}
+	</div>
+	<div>
+		Precisione: {Math.round(coords.accuracy)}m
+	</div>
+{:else}
+	Caricamento...
 {/if}
