b530ee8dbba978b575d1e1cff6a11a1724939511
ornato_t <tommy.ornato@gmail.com>
2023-01-26 18:16:34 +0100

    Mobile stop interface

--- src/routes/stop/[stop]/+page.svelte
+++ src/routes/stop/[stop]/+page.svelte
@@ -32,11 +32,13 @@
 	}
 </script>
 
-<h1 class="mb-4 text-xl font-semibold uppercase">{data.code} - {data.db.name}</h1>
-<h2 class="my-4 font-light">{data.db.description}</h2>
+<div class="sticky top-0 z-10 bg-base-100 p-4 rounded-md">
+	<h1 class="mb-4 text-xl font-semibold uppercase">{data.code} - {data.db.name}</h1>
+	<h2 class="font-light">{data.db.description}</h2>
+</div>
 
 <!-- Desktop -->
-<div class="hidden lg:grid grid-cols-4 gap-4 mt-2">
+<div class="hidden lg:grid grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 mt-2">
 	{#if api.length !== 0}
 		{#key api}
 			{#each api as pass}
@@ -82,6 +84,49 @@
 	{/if}
 </div>
 
-<div class="lg:hidden">
-	Stronzoh, ti ho detto che non funziona su mobile
+<!-- Mobile -->
+<div class="lg:hidden grid grid-cols-1 md:grid-cols-2 gap-4 mt-2 mb-6 mx-auto place-items-center">
+	{#if api.length !== 0}
+		{#key api}
+			{#each api as pass}
+				<div class="card card-compact w-[22rem] h-full bg-neutral hover:bg-neutral-focus shadow-xl">
+					<a href="/route/{pass.route}" data-sveltekit-preload-data>
+						<div class="card-body p-6">
+							{#if pass.pass.length > 0}
+								<h2 class="card-title  mb-4 grid grid-cols-4">
+									<span class="text-2xl text-left">{pass.route}</span>
+									<span class="text-sm font-light text-right col-span-3">
+										{pass.direction}
+									</span>
+								</h2>
+								<div class="justify-end">
+									<div class="w-full grid grid-cols-3">
+										{#each pass.pass as time}
+											<div class="text-left">
+												{printLocale(time)}
+											</div>
+											<div class="justify-end col-span-2 countdown font-mono">
+												<Timer {time} />
+											</div>
+										{/each}
+									</div>
+
+									{#if !pass.realTime}
+										<div class="divider mb-0 mt-4" />
+										<div class="italic text-sm w-fit mx-auto opacity-60">
+											Informazioni in tempo reale non disponibili
+										</div>
+									{/if}
+								</div>
+							{:else}
+								<p>Nessuna informazione disponibile</p>
+							{/if}
+						</div>
+					</a>
+				</div>
+			{/each}
+		{/key}
+	{:else}
+		Loading...
+	{/if}
 </div>
