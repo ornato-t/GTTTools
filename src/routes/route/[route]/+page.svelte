ddc087b6f01f716b130be7f93a3c40c1ae8be8c6
ornato_t <tommy.ornato@gmail.com>
2023-01-28 18:57:14 +0100

    Vehicle page mobile design

--- src/routes/route/[route]/+page.svelte
+++ src/routes/route/[route]/+page.svelte
@@ -61,3 +61,42 @@
 		<p>Nessuna informazione in tempo reale disponibile</p>
 	{/if}
 </div>
+
+<!-- Mobile -->
+<div class="lg:hidden grid grid-cols-1 md:grid-cols-2 gap-4 mt-2 mb-6 mx-auto place-items-center">
+	{#if data.api.length !== 0}
+		{#key data.api}
+			{#each data.api as vehicle}
+				<div
+					class="card card-compact w-[22rem] h-full bg-neutral hover:bg-neutral-focus text-neutral-content shadow-xl"
+				>
+					<a href="/vehicle/{vehicle.id}" data-sveltekit-preload-data>
+						<div class="card-body p-6">
+							<h2 class="card-title  mb-4 grid grid-cols-4">
+								<span class="text-2xl text-left">{vehicle.id}</span>
+								<span class="text-sm font-light text-right col-span-3">
+									{vehicle.vehicleType.toUpperCase()}
+								</span>
+							</h2>
+							<div class="mx-auto -mb-2">Posizione:</div>
+							<div class="mx-auto font-mono">
+								{vehicle.lat},{vehicle.lon}
+							</div>
+
+							<div class="justify-end mt-4">
+								{#if vehicle.full}
+									<div class="text-warning mx-auto w-fit my-2">Veicolo pieno!</div>
+								{/if}
+								<div class="font-mono text-sm text-end">
+									Aggiornato {relativeDate(vehicle.updated)}
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			{/each}
+		{/key}
+	{:else}
+		<p>Nessuna informazione in tempo reale disponibile</p>
+	{/if}
+</div>
