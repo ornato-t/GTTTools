6636cfc215929789ef9bbb87ae453c4bc6bff73d
ornato_t <tommy.ornato@gmail.com>
2023-03-26 00:04:33 +0100

    Update import

--- src/routes/api/stop-experimental/[stop]/+server.ts
+++ src/routes/api/stop-experimental/[stop]/+server.ts
@@ -3,7 +3,7 @@ import type { trip } from "$lib/trip";
 import type { RequestHandler } from "@sveltejs/kit";
 import GtfsRealtimeBindings from "gtfs-realtime-bindings";
 import type { Collection } from "mongodb";
-import type Long from "node_modules/long";
+import type { Long } from "mongodb";
 
 export const GET: RequestHandler = async ({ params, locals }) => {
     const { trips }: { trips: Collection<trip> } = locals;
