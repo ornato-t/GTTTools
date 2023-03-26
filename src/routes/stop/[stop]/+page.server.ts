93bb74db34d3d5aac4dcecb1d3d6d1911b36b341
ornato_t <tommy.ornato@gmail.com>
2023-03-26 22:56:56 +0200

    Update legacy stop page load to reflect DB changes

--- src/routes/stop/[stop]/+page.server.ts
+++ src/routes/stop/[stop]/+page.server.ts
@@ -1,27 +1,12 @@
 import type { stopDB } from '$lib/stopDB';
 import type { PageServerLoad } from './$types';
-import type { Collection, Document } from "mongodb";
+import type { Collection } from "mongodb";
 
 export const load: PageServerLoad = async ({ params, locals }) => {
     const { stops }: { stops: Collection<stopDB> } = locals;
     const code = parseInt(params.stop);
 
-    const aggr = [
-        { '$match': { 'code': code } },
-        { '$addFields': { 'coords': '$coordinates' } },
-        { '$project': { '_id': 0, 'coordinates': 0 } },
-        {
-            '$addFields': {
-                'coordinates.lat': { '$arrayElemAt': ['$coords', 1] },
-                'coordinates.lon': { '$arrayElemAt': ['$coords', 0] }
-            }
-        },
-        { '$project': { 'coords': 0 } },
-    ];
-
-    const res = stops.aggregate(aggr).toArray().then((arr: Document[]) => {
-        return arr[0] as stopDB;
-    }) as Promise<stopDB>;
+    const res = stops.findOne({ code }, { projection: { _id: 0 } }) as Promise<stopDB>;
 
     return {
         code,
