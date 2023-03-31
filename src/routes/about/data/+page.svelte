afae09a65a2858516b444c7bb2cdc18cde651189
ornato-t <tommy.ornato@gmail.com>
2023-03-31 08:37:04 +0200

    Static pages: privacy policy and data source

--- src/routes/about/data/+page.svelte
+++ src/routes/about/data/+page.svelte
@@ -2,7 +2,16 @@
 	export const csr = false;
 </script>
 
-<h1>Proprietà dei dati</h1>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim id numquam necessitatibus. Tenetur natus
-officiis laborum, quidem debitis error dolor omnis vero iure corporis perferendis explicabo incidunt
-magnam porro ducimus.
+<h1 class="font-bold text-xl mb-2">Proprietà dei dati</h1>
+
+<div>
+	<p class="mb-2">
+		<span class="italic">GTTTools</span> utilizza le seguenti fonti di dati:
+	</p>
+	<ol class="list-decimal">
+		<li><span>Feed GTFS trasporti GTT</span> della Città di Torino, consultabile su <a href="http://aperto.comune.torino.it/dataset/feed-gtfs-trasporti-gtt" class="link">AperTO</a> con licenza <a href="https://creativecommons.org/licenses/by/4.0/legalcode.it" class="link">Creative Commons Attribuzione 4.0 (CC BY)</a>.</li>
+		<li><span>Feed GTFS Real-Time trasporti GTT</span> della Città di Torino, consultabile su <a href="http://aperto.comune.torino.it/dataset/feed-gtfs-trasporti-gtt" class="link">AperTO</a> con licenza <a href="https://creativecommons.org/licenses/by/4.0/legalcode.it" class="link">Creative Commons Attribuzione 4.0 (CC BY)</a>.</li>
+		<li><span>API pubblica arrivi in fermata</span> di GTT, consultabile su <a href="https://gtt.to.it/cms/percorari/arrivi?palina=&bacino=" class="link">www.gtt.to.it</a>.</li>
+		<li><span>API pubblica percorsi e orari</span> di GTT, consultabile su <a href="https://gtt.to.it/cms/percorari/urbano?view=linee&bacino=U" class="link">www.gtt.to.it</a>.</li>
+	</ol>
+</div>
--- src/routes/about/privacy/+page.svelte
+++ src/routes/about/privacy/+page.svelte
@@ -2,7 +2,18 @@
 	export const csr = false;
 </script>
 
-<h1>Privacy</h1>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim id numquam necessitatibus. Tenetur natus
-officiis laborum, quidem debitis error dolor omnis vero iure corporis perferendis explicabo incidunt
-magnam porro ducimus.
+
+<h1 class="font-bold text-xl mb-2">Privacy policy</h1>
+
+<div>
+	<p class="mb-2">
+		<span class="italic">GTTTools</span> non raccoglie alcun dato appartenente agli utenti. I dati inseriti dagli utenti attraverso le barre di ricerca non vengono salvati in nessuna circostanza. Le posizioni degli utenti raccolte attraverso la ricerca tramite GPS non vengono salvate in nessuna circostanza.
+	</p>
+	<p class="mb-2">
+		Non viene fatto uso di alcun tipo di cookie, nè di prima nè di terze parti.
+	</p>
+	<p class="mb-2">
+		L'utilizzo di <span class="italic">GTTTools</span> non è soggetto a particolari limiti di età.
+	</p>
+</div>
+
