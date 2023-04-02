ca5827e0af75b2f600204e689e351ee0d69d7091
ornato_t <tommy.ornato@gmail.com>
2023-04-03 00:30:22 +0200

    Redesigning GPS loading page

--- src/routes/stop/gps/+page.svelte
+++ src/routes/stop/gps/+page.svelte
@@ -8,6 +8,12 @@
 	let errorFlag = false;
 	let errorCode: number;
 
+	let dots = '...'
+	setInterval(() => {
+		if(dots.length !== 3) dots+='.';
+		else dots = '';
+	}, 500)
+
 	onMount(() => {
 		getPosition = true;
 	});
@@ -28,21 +34,24 @@
 	options={{ enableHighAccuracy: true }}
 />
 
-{#if errorFlag}
-	<h1 class="font-bold text-xl">Errore</h1>
-	<div class="bg-error text-error-content text-lg">
-		{#if errorCode === 1}
-			Accesso alla posizione negato
-		{:else if errorCode === 2}
-			Posizione non disponibile
-		{:else if errorCode === 3}
-			Il dispositivo non ha risposto in tempo (timeout)
-		{/if}
+{#if !errorFlag}
+	<div class="mx-auto pt-44 w-fit text-center">
+		<i class='bx bxs-map bx-spin text-9xl'/>
+		<div class="w-40 mt-4 text-xl font-light">Caricamento{dots}</div>
 	</div>
-	<span
-		>Prova a usare la
-		<a href="/stop/search" class="link" data-sveltekit-preload-data> ricerca testuale</a>
-	</span>
 {:else}
-	Caricamento...
+	<h1 class="font-bold text-xl">Errore</h1>
+		<div class="bg-error text-error-content text-lg">
+			{#if errorCode === 1}
+				Accesso alla posizione negato
+			{:else if errorCode === 2}
+				Posizione non disponibile
+			{:else if errorCode === 3}
+				Il dispositivo non ha risposto in tempo (timeout)
+			{/if}
+		</div>
+		<span>
+			Prova a usare la
+			<a href="/stop/search" class="link" data-sveltekit-preload-data> ricerca testuale</a>
+		</span>
 {/if}
