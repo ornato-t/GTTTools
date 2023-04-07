10c262523cbd4040e9237c627cb96d630540a517
ornato_t <tommy.ornato@gmail.com>
2023-04-07 21:48:17 +0200

    Hide stop description if undefined

--- src/routes/stop/[stop]/+page.svelte
+++ src/routes/stop/[stop]/+page.svelte
@@ -34,7 +34,7 @@
 
 <div class="p-4">
 	<h1 class="mb-4 text-xl font-semibold uppercase">{data.code} - {data.db.name}</h1>
-	<h2 class="font-light">{data.db.description}</h2>
+	<h2 class="font-light">{data.db.description ?? ''}</h2>
 </div>
 
 <!-- Desktop -->
