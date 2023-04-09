590c5c12be4cdad376b23fc83cdd28e169831a2a
ornato_t <tommy.ornato@gmail.com>
2023-04-10 00:44:57 +0200

    Vehicle search page

--- src/routes/vehicle/search/+page.svelte
+++ src/routes/vehicle/search/+page.svelte
@@ -1,1 +1,19 @@
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum vitae alias, enim illo repellendus eaque impedit eum eligendi voluptatum consequuntur voluptatem qui velit dicta error placeat facilis corrupti. Ab, quas!
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let value = '';
+</script>
+
+<div class="form-control w-full max-w-xs mx-auto lg:mx-0">
+	<!-- svelte-ignore a11y-label-has-associated-control -->
+	<label class="label">
+		<span class="label-text">Inserisci il codice di un veicolo</span>
+	</label>
+
+	<form on:submit={() => setTimeout(() => goto(`/vehicle/${value}`), 0)}>
+		<!-- svelte-ignore a11y-autofocus -->
+		<input type="text" bind:value class="input input-bordered w-full max-w-xs" placeholder="Cerca" autofocus>
+		
+		<button class="btn mx-auto w-full mt-4" type="submit">Cerca</button>
+	</form>
+</div>
