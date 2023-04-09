e2158132c68897b132ea3f913937db189d0b969f
ornato_t <tommy.ornato@gmail.com>
2023-04-10 00:07:39 +0200

    Splitting route page load functions between client and server

--- src/routes/route/[route]/+page.server.ts
+++ src/routes/route/[route]/+page.server.ts
@@ -1,37 +1,17 @@
-import type { vehicle } from '$lib/vehicle';
 import type { routeDB } from '$lib/routeDB';
 import type { PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
 import type { Collection } from "mongodb";
 
-export const load = (async ({ params, fetch, depends, locals }) => {
+export const load = (async ({ params, locals, depends }) => {
     const code = params.route;
 
-    depends('vehicle');
+    depends('vehicleDB');
 
     return {
         code,
-        api: getRoute(code, fetch),
         db: getDB(code, locals)
     };
 }) satisfies PageServerLoad;
-
-async function getRoute(code: string, fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>) {
-    let route: Response;
-
-    try {   //Try to fetch from site first (faster)
-        route = await fetch(`/api/route/${code}`, { signal: AbortSignal.timeout(1000) });   //Send timeout if request takes longer than a seconds
-    } catch (e) {   //If it fails, resort to experimental API
-        route = await fetch(`/api/route-experimental/${code}`);
-    }
-
-    if (route.status !== 200) {
-        const err = await route.json();
-        throw error(err.status ?? 500, { message: err.message });
-    }
-
-    return route.json() as Promise<vehicle[]>;
-}
 
 async function getDB(code: string, locals: App.Locals) {
     const { routes }: { routes: Collection<routeDB> } = locals;
---
+++ src/routes/route/[route]/+page.ts
@@ -0,0 +1,30 @@
+import type { vehicle } from '$lib/vehicle';
+import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
+
+export const load = (async ({ fetch, depends, data }) => {
+    depends('vehicle');
+
+    return {
+        code: data.code,
+        api: getRoute(data.code, fetch),
+        db: data.db
+    };
+}) satisfies PageLoad;
+
+async function getRoute(code: string, fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>) {
+    let route: Response;
+
+    try {   //Try to fetch from site first (faster)
+        route = await fetch(`/api/route/${code}`, { signal: AbortSignal.timeout(1000) });   //Send timeout if request takes longer than a seconds
+    } catch (e) {   //If it fails, resort to experimental API
+        route = await fetch(`/api/route-experimental/${code}`);
+    }
+
+    if (route.status !== 200) {
+        const err = await route.json();
+        throw error(err.status ?? 500, { message: err.message });
+    }
+
+    return route.json() as Promise<vehicle[]>;
+}
