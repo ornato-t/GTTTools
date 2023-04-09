8de9cdfd494a6ebeb07ae81237f0b21c6c2a87fd
ornato_t <tommy.ornato@gmail.com>
2023-04-10 01:17:01 +0200

    Remove protocol from image credits URL It looks bad

--- src/lib/vehicleImages.ts
+++ src/lib/vehicleImages.ts
@@ -3,135 +3,135 @@ const vehicles = [
         idL: 1,
         idH: 23,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/01E-23E/16E-01-800.jpg",
-        credits: "Matteo Spina via http://www.tplitalia.it",
+        credits: "Matteo Spina via www.tplitalia.it",
         modifier: "_E"
     }, {
         idL: 30,
         idH: 49,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/30E-49E/41E-02-800.jpg",
-        credits: "Alessandro Laronga via http://www.tplitalia.it",
+        credits: "Alessandro Laronga via www.tplitalia.it",
         modifier: "_E"
     }, {
         idL: 50,
         idH: 56,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/50E-57E/53E-02-800.jpg",
-        credits: "Fabrizio Moscatelli via http://www.tplitalia.it",
+        credits: "Fabrizio Moscatelli via www.tplitalia.it",
         modifier: "_E"
     }, {
         idL: 85,
         idH: 89,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/85-89/87-01-800.jpg",
-        credits: "Chandu Belletti via http://www.tplitalia.it"
+        credits: "Chandu Belletti via www.tplitalia.it"
     }, {
         idL: 2300,
         idH: 2334,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/2300-2334/2311-01-800.jpg",
-        credits: "Christian Bizzi via http://www.tplitalia.it"
+        credits: "Christian Bizzi via www.tplitalia.it"
     }, {
         idL: 2335,
         idH: 2349,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/2335-2349/2341-01-800.jpg",
-        credits: "Fabrizio Moscatelli via http://www.tplitalia.it"
+        credits: "Fabrizio Moscatelli via www.tplitalia.it"
     }, {
         idL: 2400,
         idH: 2447,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/2400-2439/2403-02-800.jpg",
-        credits: "Lorenzo Libertazzi via http://www.tplitalia.it"
+        credits: "Lorenzo Libertazzi via www.tplitalia.it"
     }, {
         idL: 2700,
         idH: 2787,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/2700-2787/2732-01-800.jpg",
-        credits: "Alberto Viscardi via http://www.tplitalia.it"
+        credits: "Alberto Viscardi via www.tplitalia.it"
     }, {
         idL: 3000,
         idH: 3099,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/3000-3099/3023-01-800.jpg",
-        credits: "Nomen via http://www.tplitalia.it"
+        credits: "Nomen via www.tplitalia.it"
     }, {
         idL: 3300,
         idH: 3380,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/3300-3380/3363-02-800.jpg",
-        credits: "Fabrizio Moscatelli via http://www.tplitalia.it"
+        credits: "Fabrizio Moscatelli via www.tplitalia.it"
     }, {
         idL: 3400,
         idH: 3440,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/3400-3439/3401-01-800.jpg",
-        credits: "Fabio Colbatello via http://www.tplitalia.it"
+        credits: "Fabio Colbatello via www.tplitalia.it"
     }, {
         idL: 9000,
         idH: 9099,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/9000-9099/9007-01-800.jpg",
-        credits: "Lorenzo Libertazzi via http://www.tplitalia.it"
+        credits: "Lorenzo Libertazzi via www.tplitalia.it"
     }, {
         idL: 1150,
         idH: 1168,
         url: "http://www.tplitalia.it/GTT/AutobusUrbSub/1150-1168/1163-03-800.jpg",
-        credits: "Andrea Antonello via http://www.tplitalia.it"
+        credits: "Andrea Antonello via www.tplitalia.it"
     }, {
         idL: 790,
         idH: 797,
         url: "http://www.tplitalia.it/GTT/AutosnodUrbSub/790-797/796-01-800.jpg",
-        credits: "Fabrizio Moscatelli via http://www.tplitalia.it"
+        credits: "Fabrizio Moscatelli via www.tplitalia.it"
     }, {
         idL: 800,
         idH: 874,
         url: "http://www.tplitalia.it/GTT/AutosnodUrbSub/800-874/814-02-800.jpg",
-        credits: "Fabrizio Moscatelli via http://www.tplitalia.it"
+        credits: "Fabrizio Moscatelli via www.tplitalia.it"
     }, {
         idL: 930,
         idH: 989,
         url: "http://www.tplitalia.it/GTT/AutosnodUrbSub/930-989/966-03-800.jpg",
-        credits: "Alberto Schilirò via http://www.tplitalia.it"
+        credits: "Alberto Schilirò via www.tplitalia.it"
     }, {
         idL: 990,
         idH: 1014,
         url: "http://www.tplitalia.it/GTT/AutosnodUrbSub/990-1014/1001-01-800.jpg",
-        credits: "Alessio Pedretti via http://www.tplitalia.it"
+        credits: "Alessio Pedretti via www.tplitalia.it"
     }, {
         idL: 1020,
         idH: 1089,
         url: "http://www.tplitalia.it/GTT/AutosnodUrbSub/1020-1089/1044-01-800.jpg",
-        credits: "Alberto Viscardi via http://www.tplitalia.it"
+        credits: "Alberto Viscardi via www.tplitalia.it"
     }, {
         idL: 1280,
         idH: 1306,
         url: "http://www.tplitalia.it/GTT/AutosnodUrbSub/1280-1306/1303-01-800.jpg",
-        credits: "Alessio Pedretti via http://www.tplitalia.it"
+        credits: "Alessio Pedretti via www.tplitalia.it"
     }, {
         idL: 1350,
         idH: 1396,
         url: "http://www.tplitalia.it/GTT/AutosnodUrbSub/1350-1383/1362-02-800.jpg",
-        credits: "Lorenzo Libertazzi via http://www.tplitalia.it"
+        credits: "Lorenzo Libertazzi via www.tplitalia.it"
     }, {
         idL: 2801,
         idH: 2902,
         url: "http://www.tplitalia.it/GTT/TramUrbani/2858-2902/2896-01-800.jpg",
         info: "https://www.tramditorino.it/tram_serie_2800.htm",
-        credits: "Alberto Viscardi via http://www.tplitalia.it"
+        credits: "Alberto Viscardi via www.tplitalia.it"
     }, {
         idL: 5000,
         idH: 5053,
         url: "http://www.tplitalia.it/GTT/TramUrbani/5000-5053/5039-01-800.jpg",
         info: "https://www.tramditorino.it/tram_serie_5000.htm",
-        credits: "Christian Bizzi via http://www.tplitalia.it"
+        credits: "Christian Bizzi via www.tplitalia.it"
     }, {
         idL: 6000,
         idH: 6005,
         url: "http://www.tplitalia.it/GTT/TramUrbani/6000-6005/6003-02-800.jpg",
         info: "https://www.tramditorino.it/tram_serie_6000.htm",
-        credits: "Nomen via http://www.tplitalia.it"
+        credits: "Nomen via www.tplitalia.it"
     }, {
         idL: 6006,
         idH: 6054,
         url: "http://www.tplitalia.it/GTT/TramUrbani/6006-6054/6006-01-800.jpg",
         info: "https://www.tramditorino.it/tram_serie_6000.htm",
-        credits: "Christian Bizzi via http://www.tplitalia.it"
+        credits: "Christian Bizzi via www.tplitalia.it"
     }, {
         idL: 8000,
         idH: 8100,
         url: "https://www.tramditorino.it/img/tram8000.jpg",
         info: "https://www.tramditorino.it/tram_serie_8000.htm",
-        credits: "Tram di Torino via https://www.tramditorino.it"
+        credits: "Tram di Torino via www.tramditorino.it"
     },
 ]satisfies vehicle[];
 
@@ -141,28 +141,28 @@ const vehiclesSup = [   //Vehicles on the Superga-Sassi special route, these IDs
         idH: 1,
         url: "https://www.tramditorino.it/gallery/tram/D1-sassi2.jpg",
         info: "https://www.tramditorino.it/superga_D1.htm",
-        credits: "Tram di Torino via https://www.tramditorino.it",
+        credits: "Tram di Torino via www.tramditorino.it",
         modifier: "D_"
     }, {
         idL: 2,
         idH: 3,
         url: "https://www.tramditorino.it/gallery/tram/D2-binario2.jpg",
         info: "https://www.tramditorino.it/superga_D2-D3.htm",
-        credits: "Tram di Torino via https://www.tramditorino.it",
+        credits: "Tram di Torino via www.tramditorino.it",
         modifier: "D_"
     }, {
         idL: 11,
         idH: 12,
         url: "https://www.tramditorino.it/gallery/tram/D12sassi.jpg",
         info: "https://www.tramditorino.it/superga_D11-D14.htm",
-        credits: "Tram di Torino via https://www.tramditorino.it",
+        credits: "Tram di Torino via www.tramditorino.it",
         modifier: "D_"
     }, {
         idL: 13,
         idH: 14,
         url: "https://www.tramditorino.it/gallery/tram/D14.jpg",
         info: "https://www.tramditorino.it/superga_D11-D14.htm",
-        credits: "Tram di Torino via https://www.tramditorino.it",
+        credits: "Tram di Torino via www.tramditorino.it",
         modifier: "D_"
     }
 ]satisfies vehicle[];
