427f47daa71effc1717a2f55da8118a102d92b1e
ornato_t <tommy.ornato@gmail.com>
2023-04-10 01:05:01 +0200

    Skeleton loader and promise streaming for vehicle finder API

--- src/routes/vehicle/[vehicle]/+page.svelte
+++ src/routes/vehicle/[vehicle]/+page.svelte
@@ -2,6 +2,12 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let dots = '...'
+	setInterval(() => {
+		if(dots.length !== 3) dots+='.';
+		else dots = '';
+	}, 500)
 </script>
 
 
@@ -21,3 +27,14 @@
 		Maggiori informazioni al seguente <a class="link" href={data.info}>link</a>.
 	</div>
 {/if}
+
+<div class="mb-16">
+	{#await data.route.promise}
+			<div class="mx-auto pt-44 w-fit text-center">
+				<i class='bx bx-loader-circle bx-spin text-8xl'/>
+				<div class="w-40 mt-4 text-xl font-light">Ricerca linea{dots}</div>
+			</div>
+	{:then route} 
+		<!-- {route} -->
+	{/await}
+</div>
--- src/routes/vehicle/[vehicle]/+page.ts
+++ src/routes/vehicle/[vehicle]/+page.ts
@@ -8,7 +8,7 @@ export const load = (async ({ params }) => {
     const res = getVehicle(code);
 
     if (res === null) throw error(404, 'No matching vehicle found');
-    
+
     let outId: string;
     if (res?.modifier !== undefined) outId = res.modifier.replace('_', `${res.code}`);
     else outId = `${res.code}`;
@@ -19,5 +19,13 @@ export const load = (async ({ params }) => {
         url: res.url,
         credits: res.credits,
         info: res.info ?? '',
+        route: {
+            promise: findRoute(outId)
+        }
     };
 }) satisfies PageLoad;
+
+//TODO: poll every route of the vehicle API until a match is found. This is probably going to need a proxy of its own
+async function findRoute(id: string) {
+    return fetch('https://hub.dummyapis.com/delay?seconds=60');
+}
