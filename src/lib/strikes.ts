d887c34bc2b4c4f3beff910ad3c1b660b65c0bde
ornato_t <tommy.ornato@gmail.com>
2023-04-12 23:29:27 +0200

    Adding API endpoint for transportation strikes

---
+++ src/lib/strikes.ts
@@ -0,0 +1,50 @@
+export interface strike {
+    mode: string,
+    dateEnd: Date,
+    sector: string,
+    scope: string,
+    // province: string,    //Only returning national ones and Piedmont ones
+    unions: string,
+    category: string,
+    dateSubmission: Date,
+}
+
+
+export interface strikesFeed {
+    items: strikeFeed[],
+    image: feedImage,
+    title: string,
+    description: string,
+    pubDate: string,    //Date in the format: 'Wed, 12 Apr 2023 19:57:33 +0000'
+    generator: string,
+    link: string,
+}
+
+export interface strikeInfoFeed {
+    modalità: string,
+    "Data fine": string,
+    Settore: string,
+    Rilevanza: string,
+    Regione: string,
+    Provincia: string,
+    Sindacati: string,
+    "Categoria interessata": string,
+    "Data proclamazione": string,
+    "Data ricezione": string,
+}
+
+interface strikeFeed {
+    title: string,
+    link: string,
+    pubDate: string,
+    content: string,
+    contentSnippet: string,
+    guid: string,
+    isoDate: string //pubDate in ISO format, easier for parsing but not too useful
+}
+
+interface feedImage {
+    link: string,
+    url: string,
+    title: string
+}
---
+++ src/routes/api/strikes/+server.ts
@@ -0,0 +1,39 @@
+import Parser from "rss-parser";
+import { DateTime } from "luxon";
+import type { RequestHandler } from "@sveltejs/kit";
+import type { strike, strikesFeed, strikeInfoFeed } from "$lib/strikes";
+
+const parser = new Parser();
+
+export const GET: RequestHandler = async () => {
+    const feed = await parser.parseURL('http://scioperi.mit.gov.it/mit2/public/scioperi/rss') as strikesFeed;   //Fetch strikes feed
+
+    const out: Array<strike> = [];
+    const regex = /(.*): (.*)/;
+
+    for (const strike of feed.items) {
+        const parsed = strike.contentSnippet.split('\n').map(line => {
+            const data = line.match(regex);     //Extract keys and values
+
+            if (data === null) return '';
+
+            return `"${data[1]}": "${data[2]}"`;    //Remap to JSON like syntax
+        });
+
+        const res = await JSON.parse('{' + parsed + '}') as strikeInfoFeed; //Add delimiters and parse as JSON
+
+        if ((res.Regione.toLowerCase() === 'piemonte' || res.Regione.toLowerCase() === ' italia') && res.Settore !== 'Aereo') {
+            out.push({
+                mode: res.modalità,
+                dateEnd: DateTime.fromFormat(res["Data fine"], 'dd/MM/y').toJSDate(),
+                sector: res.Settore,
+                scope: res.Rilevanza,
+                unions: res.Sindacati,
+                category: res["Categoria interessata"],
+                dateSubmission: DateTime.fromFormat(res["Data proclamazione"], 'dd/MM/y').toJSDate(),
+            });
+        }
+    }
+
+    return new Response(JSON.stringify(out));
+}
