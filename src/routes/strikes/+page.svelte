a6139ae062ee872154da3ee7a2808fbfaecc08fa
ornato-t <tommy.ornato@gmail.com>
2023-04-14 08:58:02 +0200

    Finished strikes page styling

--- src/routes/strikes/+page.svelte
+++ src/routes/strikes/+page.svelte
@@ -8,24 +8,21 @@
         return new Date(d).toLocaleDateString()
     }
 </script>
+<div class="p-4">
+	<h1 class="mb-2 text-xl font-semibold">Scioperi programmati </h1>
+</div>
 
-<!-- TODO: adapt loading component -->
-<!-- TODO: mobile -->
-
-<!-- Desktop -->
-<div class="hidden lg:grid grid-cols-2 gap-4 mt-2">
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-2 mb-10 mx-2 lg:mx-0">
     {#await data.api.promise}
 		<Loading />
-		<!-- <Loading />
-		<span class="hidden xl:block">
-			<Loading />
-		</span>
-		<span class="hidden 2xl:block">
-			<Loading />
-		</span> -->
+		<Loading />
+		<Loading />
+		<Loading />
+		<Loading />
+		<Loading />
     {:then strikesList} 
         {#each strikesList as strike}
-            <div class="card w-full h-full bg-neutral hover:bg-neutral-focus text-neutral-content shadow-xl">
+            <div class="card card-compact w-full h-full bg-neutral hover:bg-neutral-focus text-neutral-content shadow-xl">
                 <div class="card-body p-6">
                 <h2 class="card-title text-2xl text-left">{printDate(strike.dateEnd)}</h2>
                     <h3 class="card-title text-sm font-light text-right">
@@ -42,11 +39,11 @@
                             <div class="text-left">
                                 Settore: <span class="font-mono">{strike.sector}</span> 
                             </div>
-                            <span class="text-sm grid grid-cols-3 mt-2">
+                            <span class="text-xs grid grid-cols-2 mt-2">
                                 <div class="text-left">
                                     Proclamazione: <span class="font-mono">{printDate(strike.dateSubmission)}</span> 
                                 </div>
-                                <div class="text-right col-span-2">
+                                <div class="text-right">
                                     Sindacati: <span class="font-mono">{strike.unions}</span> 
                                 </div>
                             </span>
@@ -57,76 +54,3 @@
         {/each}
     {/await}
 </div>
-
-<!-- Mobile -->
-<div class="lg:hidden grid grid-cols-1 md:grid-cols-2 gap-4 mt-2 mb-6 mx-auto place-items-center">
-    {#await data.api.promise}
-        Caricamento...
-    {:then strikeList} 
-        {#each strikeList as strike}
-            <div class="rounded-xl">
-                <div>
-                    Categoria: <span class="font-mono">{strike.category}</span>
-                </div>
-                <div>
-                    Modalità: <span class="font-mono">{strike.mode}</span>
-                </div>
-                <div>
-                    Ambito: <span class="font-mono">{strike.scope}</span>
-                </div>
-                <div>
-                    Data fine: <span class="font-mono">{printDate(strike.dateEnd)}</span>
-                </div>
-                <div>
-                    Settori coinvolti: <span class="font-mono">{strike.sector}</span>
-                </div>
-                <div>
-                    Sindacati coinvolti: <span class="font-mono">{strike.unions}</span>
-                </div>
-                <div>
-                    Data proclamazione: <span class="font-mono">{printDate(strike.dateSubmission)}</span>
-                </div>
-            </div>
-        {/each}
-    {/await}
-	<!-- {#if api.length === 0}
-	<div class="mx-4 grid gap-y-4">
-		<Loading />
-		<Loading />
-		<Loading />
-		<Loading />
-	</div>
-	{:else} 
-			{#each api as pass}
-				<div class="card card-compact w-[22rem] h-full bg-neutral hover:bg-neutral-focus text-neutral-content shadow-xl">
-					<a href="/route/{pass.route}" data-sveltekit-preload-data>
-						<div class="card-body p-6">
-							{#if pass.pass.length > 0}
-								<h2 class="card-title mb-4 grid grid-cols-4">
-									<span class="text-2xl text-left">{pass.route}</span>
-									<span class="text-sm font-light text-right col-span-3">
-										{pass.direction}
-									</span>
-								</h2>
-								<div class="justify-end">
-									<div class="w-full grid grid-cols-3">
-										{#each pass.pass as time}
-											<div class="text-left {time.realTime ? '' : 'opacity-50'}">
-												{printLocale(time.time)}
-											</div>
-											<div class="justify-end col-span-2 countdown font-mono {time.realTime ? '' : 'opacity-50'}">
-												<Timer time={time.time} />
-											</div>
-										{/each}
-									</div>
-								</div>
-							{:else}
-								<p>Nessuna informazione disponibile</p>
-							{/if}
-						</div>
-					</a>
-				</div>
-			{/each}
-	{/if} -->
-</div>
-
--- src/routes/strikes/loading.svelte
+++ src/routes/strikes/loading.svelte
@@ -1,47 +1,23 @@
-<!-- Desktop -->
-<span class="hidden lg:block">
-	<div class="bg-neutral animate-pulse rounded-2xl h-48 w-96 p-6">
-		<div class="h-full grid grid-rows-6 place-items-center md:place-items-start md:gap-x-2">
-			<div class="my-auto w-full rounded-sm md:rounded-none row-span-3 grid grid-cols-2">
-				<div class="bg-base-100 w-12 h-12  rounded-xl" />
-				<div class="bg-base-100 w-40 h-2 my-auto rounded-xl justify-self-end" />
-			</div>
-			<div class="my-auto w-full h-2 md:pl-1 grid grid-cols-2">
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-start" />
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-end" />
-			</div>
-			<div class="my-auto w-full h-2 md:pl-1  grid grid-cols-2">
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-start" />
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-end" />
-			</div>
-			<div class="my-auto w-full h-2 md:pl-1  grid grid-cols-2">
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-start" />
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-end" />
-			</div>
+<div class="bg-neutral animate-pulse rounded-2xl h-60 lg:h-52 w-full p-4">
+	<div class="h-full grid grid-rows-8 place-items-center md:place-items-start md:gap-x-2">
+		<div class="my-auto w-full rounded-sm md:rounded-none grid-cols-2">
+			<div class="bg-base-100 w-32 h-6 rounded-xl" />
+		</div>
+		<div class="my-auto w-full h-2 md:pl-1 grid grid-cols-2">
+			<div class="bg-base-100 w-40 h-2 my-auto rounded-xl justify-self-start" />
+		</div>
+		<div class="my-auto w-full h-2 md:pl-1 grid grid-cols-2">
+			<div class="bg-base-100 w-60 h-2 my-auto rounded-xl justify-self-start" />
+		</div>
+		<div class="my-auto w-full h-2 md:pl-1  grid grid-cols-2">
+			<div class="bg-base-100 w-20 h-full rounded-xl justify-self-start" />
+		</div>
+		<div class="my-auto w-full h-2 md:pl-1  grid grid-cols-2">
+			<div class="bg-base-100 w-20 h-full rounded-xl justify-self-start" />
+		</div>
+		<div class="my-auto w-full h-2 md:pl-1  grid grid-cols-2">
+			<div class="bg-base-100 w-20 h-full rounded-xl justify-self-start" />
+			<div class="bg-base-100 w-40 h-full rounded-xl justify-self-end" />
 		</div>
 	</div>
-</span>
-
-<!-- Mobile -->
-<span class="lg:hidden">
-	<div class="bg-neutral animate-pulse rounded-2xl h-36 w-[22.5rem] p-4">
-		<div class="h-full -mt-1 grid grid-rows-6 place-items-center md:place-items-start md:gap-x-2">
-			<div class="my-auto mb-2 w-full rounded-sm md:rounded-none row-span-3 grid grid-cols-2">
-				<div class="bg-base-100 w-12 h-12  rounded-xl" />
-				<div class="bg-base-100 w-40 h-2 my-auto rounded-xl justify-self-end" />
-			</div>
-			<div class="my-auto pl-1 w-full h-2 md:pl-1 grid grid-cols-2">
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-start" />
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-end" />
-			</div>
-			<div class="my-auto pl-1 w-full h-2 md:pl-1  grid grid-cols-2">
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-start" />
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-end" />
-			</div>
-			<div class="my-auto pl-1 w-full h-2 md:pl-1  grid grid-cols-2">
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-start" />
-				<div class="bg-base-100 w-10 h-full rounded-xl justify-self-end" />
-			</div>
-		</div>
-	</div>
-</span>
+</div>
