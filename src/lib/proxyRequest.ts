b4cadc9cbdbe04f73c51594a11c66f47e7d11abe
ornato_t <tommy.ornato@gmail.com>
2023-04-17 01:05:44 +0200

    Trying to avoid flash of invisible text GPT generated code

--- src/lib/proxyRequest.ts
+++ src/lib/proxyRequest.ts
@@ -2,7 +2,7 @@ import { dev } from "$app/environment";
 
 export default async function pfetch(url: string){
     //Vercel dev environment
-    // const base = "https://gtt-tools-ornato-t.vercel.app";
+    // return fetch("https://gtt-tools-ornato-t.vercel.app" + url)
 
     //Localhost
     const base = dev ? "http://localhost:5173" : "https://tools.gtt.cx";
