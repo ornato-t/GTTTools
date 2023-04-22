6abee4fab786d11965eeb291c3f6b6f3a2a22e82
ornato_t <tommy.ornato@gmail.com>
2023-04-22 11:55:19 +0200

    Adding refresh (invalidate) to route map page Pretty printing available data, just a useless flex

--- src/routes/route/[route]/map/+page.svelte
+++ src/routes/route/[route]/map/+page.svelte
@@ -3,16 +3,32 @@
     import { onMount } from 'svelte';
 	import { invalidate } from '$app/navigation';
     import type { PageData } from "./$types";
-    import type { Marker, LatLngTuple, Map } from "leaflet";
-    import type { stopDB } from "$lib/stopDB"
+    // import type { Marker, LatLngTuple, Map } from "leaflet";
 
     export let data: PageData;
+    
+    onMount(() => setInterval(() => invalidate('vehicle'), 1000));
 </script>
 
-<div>
-    {JSON.stringify(data)}
-    <br>
+<div class="mb-6">
+    <div class="text-lg font-bold">Codice:</div>
+    <div>{data.code}</div>
+    <div class="text-lg font-bold">Dati da database:</div>
+    <div>{JSON.stringify(data.db)}</div>
+    <div class="text-lg font-bold">Dati in tempo reale:</div>
+    {#each data.api as vehicle}
+        <div>{JSON.stringify(vehicle)}</div>
+    {/each}
+    <div class="text-lg font-bold">Percorso (punti)</div>
+    <div>
+        {#each data.shape as point}
+            <div>{point}</div>
+        {/each}
+    </div>
     {#await data.stops.promise then stops}
-        {JSON.stringify(stops)}
+        <div class="text-lg font-bold">Fermate:</div>
+        {#each stops as stop}
+            <div>{JSON.stringify(stop)}</div>
+        {/each}
     {/await}
 </div>
