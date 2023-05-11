b87af59dfcdd3e1048b626d11e27bd3aeadf494d
ornato-t <tommy.ornato@gmail.com>
2023-05-11 11:42:44 +0200

    Make vehicle search numeric only

--- src/routes/vehicle/search/+page.svelte
+++ src/routes/vehicle/search/+page.svelte
@@ -18,7 +18,7 @@
 	<!-- For some reason not using the timeout here breaks everyting -->
 	<form on:submit={() => setTimeout(() => goto(`/vehicle/${value}`), 0)}>
 		<!-- svelte-ignore a11y-autofocus -->
-		<input type="text" bind:value class="input input-bordered w-full max-w-xs" placeholder="Cerca" autofocus>
+		<input type="text" bind:value class="input input-bordered w-full max-w-xs" placeholder="Cerca" autofocus inputmode="numeric">
 		
 		<button class="btn mx-auto w-full mt-4" type="submit">Cerca</button>
 	</form>
