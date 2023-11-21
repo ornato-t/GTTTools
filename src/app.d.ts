32b773949c40d1054d7d762d8c4cb41b88bc5cc4
Tommaso Ornato <tommy.ornato@gmail.com>
2023-11-21 23:40:49 +0100

    Fixing Mongo TS errors

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,14 +1,24 @@
+import type { routeDB } from '$lib/routeDB';
+import type { stopDB } from '$lib/stopDB';
+import type { trip } from '$lib/trip';
 import * as L from 'leaflet';
 import * as LP from 'leaflet-polylineoffset';
+import type { Collection } from 'mongodb';
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	interface Locals {stops, routes, trips}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+declare global {
+	namespace App {
+		interface Locals {
+			stops: Collection<stopDB>,
+			routes: Collection<routeDB>,
+			trips: Collection<trip>,
+		}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
+	}
 }
 
 declare module 'leaflet-polylineoffset';
