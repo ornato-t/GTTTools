0860a9847a3e399e0086b2773bf3081041516a42
Tommaso Ornato <tommy.ornato@gmail.com>
2024-06-05 13:55:14 +0200

    Removing legacy request timeout on fallback route API request

--- src/lib/poll/route.ts
+++ src/lib/poll/route.ts
@@ -3,8 +3,6 @@ import { error } from "@sveltejs/kit";
 import { DateTime } from "luxon"
 import GtfsRealtimeBindings from "gtfs-realtime-bindings";
 
-const TIMEOUT = 1000;   //If GTT query takes longer than this, switch to GTT API
-
 export async function poll(codeIn: string) {
     const code = parseRouteCode(codeIn);
 
@@ -37,19 +35,14 @@ function parseRouteCode(codeIn: string): string {
 
 //Polls a route on the GTT website API endpoint
 async function pollRoute(route: string) {
-    const controller = new AbortController()
-    const signal = controller.signal
     const url = `https://www.gtt.to.it/cms/components/com_gtt/views/percorsi/tmpl/proxydaslinea.php?serviceName=GetVeicoliPerLineaJson&linea=${route}`;
     const options = {
         method: 'GET',
         headers: {
             Referer: `https://www.gtt.to.it/cms/percorari/urbano`,
             mode: 'no-cors' as RequestMode,
-        },
-        signal
+        }
     };
-
-    setTimeout(() => controller.abort(), TIMEOUT);  //Abort after TIMEOUT ms
 
     const response = await fetch(url, options);
     const vehiclesWeb: vehicleWeb[] | null = await response.json();
